029e80fcfc46de9a3e8ac3d6de81748898a7f11e
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-07-22 18:13:55 +0300

    5

--- src/js/render-menu.js
+++ src/js/render-menu.js
@@ -1,7 +1,8 @@
-import menuData from '..menu.json';
+import menuData from './menu.json';
 import menuItem from '../templates/menu-list-item.hbs';
 
 const menuListEl = document.querySelector('.js-menu');
 
 const murkup = menuItem(menuData);
 menuListEl.insertAdjacentHTML('beforeend', markup);
+console.log(menuData);
