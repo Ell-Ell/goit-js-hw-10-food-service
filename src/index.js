b6d7f2f1a68b33fb3dcdefd6e605887997956928
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-07-22 18:58:22 +0300

    12
    вроде разобрались с путями

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './sass/main.scss';
+import './js/render-menu.js';
+import './js/theme-switch.js';
--- src/js/index.js
+++
@@ -1,3 +0,0 @@
-import './sass/main.scss';
-import './render-menu.js';
-import './theme-switch.js';
